5efbb6f438e7e773989ecf5bc4de43dba1fffea1
inpink <108166692+inpink@users.noreply.github.com>
2023-04-20 15:39:27 +0900

    Create BOJ_24263.java

---
+++ bronze4/BOJ_24263.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main { // class name은 Main 이어야 함
+	
+	public static void main(String[] args) throws IOException { // 예외처리 꼭 해줘야 함
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write(br.readLine());
+		bw.write("\n");
+		bw.write("1");
+		bw.flush(); bw.close();
+
+	}
+	
+
+}
