70aeaaedbe06fefa835f5a614e72c59cd63aa5fe
inpink <108166692+inpink@users.noreply.github.com>
2023-05-25 02:08:35 +0900

    Update BOJ_13985.java

--- bronze4/BOJ_13985.java
+++ bronze4/BOJ_13985.java
@@ -19,7 +19,24 @@ public class Main { //class명 Main 필수
 
         if (Integer.parseInt(array[0]) + Integer.parseInt(array[2]) == Integer.parseInt(array[4])) bw.write("YES");
         else bw.write("NO");
+        
+        /* 정규식 1
+        String str3 = "1 + 2 = 3";
+        Pattern p2 = Pattern.compile("[0-9]+");
+        Matcher m2 = p2.matcher(str3);
 
+        while (m2.find()) {
+            System.out.println(m2.group()); //1  2  3
+        }
+        */
+        
+        /* 정규식 2
+        String txt4 = "1 + 2 = 3";
+        String[] txts2 = txt4.split("[+= ]+"); //  +,=,' '을 기준으로 분할. +을 달아줬기에 +=  이 여러 개 올수있음을 알려줌 
+        System.out.println(txts2[0]); //1
+        System.out.println(txts2[1]); //2
+        System.out.println(txts2[2]); //3
+        */
 
         bw.flush(); bw.close();
     }
