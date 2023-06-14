c60f5555f0f9cf089c72239beeee0a4b9a8e38a9
inpink <108166692+inpink@users.noreply.github.com>
2023-06-15 02:29:42 +0900

    Create 13297.java

---
+++ bronze3/13297.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+
+public class Main { //class명 Main 필수
+    public static void main(String[] args) throws IOException { //예외 처리 필수
+
+        //Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n= Integer.parseInt(br.readLine());
+
+        for (int i=0; i<n; i++){
+            bw.write(br.readLine().length()+"\n");
+        }
+
+        //OUTPUT
+
+        bw.flush();
+        bw.close();
+    }
+
+
+}
