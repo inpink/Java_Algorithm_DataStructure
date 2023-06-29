a7cbc3d22f8fff1c280e408ad44f5b85429bb042
inpink <108166692+inpink@users.noreply.github.com>
2023-06-30 05:30:09 +0900

    Create 11005.java

---
+++ bronze1/11005.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main { //class명 Main 필수
+    public static void main(String[] args) throws IOException { //예외 처리 필수
+
+        //Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int s=Integer.parseInt(st.nextToken());
+        int i= Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.toString(s,i).toUpperCase());
+        /*bw.flush();
+        bw.close();*/
+    }
+
+
+}
