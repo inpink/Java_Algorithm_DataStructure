77f6e804e8532361406fc78fcd0941b368bf8677
inpink <108166692+inpink@users.noreply.github.com>
2023-07-10 01:02:31 +0900

    Create 24264.java

---
+++ bronze3/24264.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main { //class명 Main 필수
+    public static void main(String[] args) throws IOException { //예외 처리 필수
+
+        //Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n= Integer.parseInt(br.readLine());
+
+        bw.write(Long.toString(n*n)+"\n");
+        bw.write("2");
+        bw.flush();
+        bw.close();
+    }
+
+
+}
