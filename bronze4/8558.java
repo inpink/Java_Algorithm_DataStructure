8c221947ec7bf309d3f937c5891a530ebdc02b01
inpink <108166692+inpink@users.noreply.github.com>
2023-10-10 03:03:07 +0900

    Create 8558.java

---
+++ bronze4/8558.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n= Integer.parseInt(br.readLine());
+        int ans=1;
+        for (int i=2; i<=n; i++) ans= ans*i%10;
+        
+        System.out.println(ans);
+    }
+}
